ac0379dba1d69fe7e68180fff9b65cd004d4644a
Dmitry Gening <dmgening@me.com>
2018-02-15 18:08:39 +0300

    Expected test failure, but pytest working fine now

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
-from cuve.order_service import application_factory
+from cuve.order_service.app import application_factory
+from cuve.order_service.config import load_config, ConfigSchema
 
 @pytest.fixture
-def client(loop, test_client):
-    app = application_factory()
+def client(request, loop, test_client):
+    config = load_config(ConfigSchema, './etc/config/development.yml')
+    app = application_factory(config, loop)
     client = test_client(app)
     return loop.run_until_complete(client)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,8 @@
 
-def test_login(client):
-    resp = client.post('/api/v1.0/auth/login')
-    assert resp.status_code == 200, f'Invalid response code. {resp}'
+async def test_login(client):
+    resp = await client.post('/api/v1.0/auth/login')
+    assert resp.status == 200, f'Invalid response code. {resp}'
 
-def test_logut(client):
-    resp = client.post('/api/v1.0/auth/logout')
-    assert resp.status_code == 200, f'Invalid response code. {resp}'
+async def test_logut(client):
+    resp = await client.post('/api/v1.0/auth/logout')
+    assert resp.status == 200, f'Invalid response code. {resp}'
