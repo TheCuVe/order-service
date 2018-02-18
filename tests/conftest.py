f0f9dcaea1c63e5c370108d5d0c4c05311fd0a10
Dmitry Gening <dgening@earthshaker.lan>
2018-02-18 11:08:15 +0300

    Added seprate app config for travis CI. Splited script to stages.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,15 @@ import pytest
 from cuve.order_service.app import application_factory
 from cuve.order_service.config import load_config, ConfigSchema
 
+
+def pytest_addoption(parser):
+    parser.addoption('--config', action="store",
+                     default='./etc/config/development.yml')
+
+
 @pytest.fixture
 def client(request, loop, test_client):
-    config = load_config(ConfigSchema, './etc/config/development.yml')
+    config = load_config(ConfigSchema, request.config.getoption('--config'))
     app = application_factory(config, loop)
     client = test_client(app)
     return loop.run_until_complete(client)
